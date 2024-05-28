5b06296a389f436cbcd521de0b641f24b34f009c
CatalinaDolhare <cdolhare@udesa.edu.ar>
2024-05-28 09:08:38 -0300

    grafico

--- src/main_alu.py
+++ src/main_alu.py
@@ -23,8 +23,15 @@ def main():
 
 	nodos_retiro = [node for node in G.nodes() if G.nodes[node]['bipartite'] == 0]
 	nodos_tigre = [node for node in G.nodes() if G.nodes[node]['bipartite'] == 1]
+	node_colors = ['#7878ff' if node[1] == 'D' else '#ff5757' for node in G.nodes()]
+	edges_colors = ['blue' if node[1] == 'D' else 'red' for node in G.nodes()]
+	node_labels = {node: f"{node[0]}" for node in G.nodes()}
 
-	nx.draw(G, with_labels=True, font_weight='bold')
+	pos = nx.bipartite_layout(G, nodes=nodos_retiro)
+
+	nx.draw(G, pos, with_labels=True, font_weight='bold', node_color =  node_colors, node_shape='s', node_size=700, edge_color=edges_colors, labels = node_labels)
+	print(G.nodes)
+	
 	plt.show()
 
 if __name__ == "__main__":
