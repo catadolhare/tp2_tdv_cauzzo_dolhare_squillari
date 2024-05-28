c40da392e7d4d5697b4b145d95c5bd6fb75e7d0d
CatalinaDolhare <cdolhare@udesa.edu.ar>
2024-05-28 14:50:20 -0300

    crear grafo con datos del json

--- src/main_alu.py
+++ src/main_alu.py
@@ -13,7 +13,12 @@ def main():
 
 	servicios_retiro = []
 	servicios_tigre = []
+	nodos_retiro = []
+	nodos_tigre = []
 	ids = list(data["services"].keys())
+	u = data["rs_info"]["max_rs"]
+	capacidad_vagon = data["rs_info"]["capacity"]
+	aristas_servicios = []
 
 	for i in range(len(ids)):
 		origen = data["services"][ids[i]]["stops"][0]
@@ -22,13 +27,51 @@ def main():
 		if (origen["station"] == "Retiro" and destino["station"] == "Tigre"):
 			servicios_retiro.append((i+1, origen["time"], origen["type"], data["services"][ids[i]]["demand"]))
 			servicios_tigre.append((i+1, destino["time"], destino["type"], data["services"][ids[i]]["demand"]))
+			nodos_retiro.append(origen["time"])
+			nodos_tigre.append(destino["time"])
 		else:
 			servicios_tigre.append((i+1, origen["time"], origen["type"], data["services"][ids[i]]["demand"]))
 			servicios_retiro.append((i+1, destino["time"], destino["type"], data["services"][ids[i]]["demand"]))
+			nodos_retiro.append(destino["time"])
+			nodos_tigre.append(origen["time"])
 	
+		l = data["services"][ids[i]]["demand"][0] / capacidad_vagon #cota inferior para cada servicio
+		arista = (origen["time"], destino["time"], {'capacity': u, 'cost': 0, 'demand': l})
+		aristas_servicios.append(arista)
+
+	nodos_retiro.sort()
+	nodos_tigre.sort()
+
+	for i in range(len(nodos_retiro) - 1):
+		arista = (nodos_retiro[i], nodos_retiro[i+1], {'capacity': float('inf'), 'cost': 0, 'demand': 0})
+		aristas_servicios.append(arista)
+	
+	for i in range(len(nodos_tigre) - 1):
+		arista = (nodos_tigre[i], nodos_tigre[i+1], {'capacity': float('inf'), 'cost': 0, 'demand': 0})
+		aristas_servicios.append(arista)
+
+	if nodos_retiro:
+		aristas_servicios.append((nodos_retiro[-1], nodos_retiro[0], {'capacity': float('inf'), 'cost': 1, 'demand': 0}))
+	if nodos_tigre:
+		aristas_servicios.append((nodos_tigre[-1], nodos_tigre[0], {'capacity': float('inf'), 'cost': 1, 'demand': 0}))
+
 
 	print("Retiro", servicios_retiro)
 	print("Tigre", servicios_tigre)
+	print("Nodos Retiro", nodos_retiro)
+	print("Nodos Tigre", nodos_tigre)
+	print("Aristas", aristas_servicios)
+
+	G = nx.DiGraph()
+	G.add_nodes_from(nodos_retiro, bipartite=0)
+	G.add_nodes_from(nodos_tigre, bipartite=1)
+	G.add_edges_from(aristas_servicios)
+
+	pos = nx.bipartite_layout(G, nodes=nodos_retiro)
+
+	nx.draw(G, pos, with_labels=True, font_weight='bold')
+	
+	plt.show()
 '''
 	G = nx.DiGraph()
 
